c36689bf603e509005c68a2dd52f594e0613c4e6
James-Ukoli <thejamesukoli@gmail.com>
2023-08-07 20:22:44 -0500

    Projects section replaces Hobbies

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import React from "react";
 import Navigation from "./sections/Navigation";
 import HomePage from "./sections/HomePage";
 import Skills from "./sections/Skills";
-import Hobbies from "./sections/Hobbies";
 import AboutMe from "../src/sections/AboutMe";
 import Contact from "../src/sections/ContactMe";
 import PlayButton from "./features/PlayButton";
+import Projects from "./sections/Projects";
 
 // 1) Play Button/Audio Commentary of Profile
 function App() {
@@ -18,8 +18,8 @@ function App() {
         <Navigation />
         <HomePage />
         <AboutMe />
+        <Projects />
         <Skills />
-        <Hobbies />
         <Contact />
       </div>
     </>
--- src/sections/Projects.js
+++ src/sections/Projects.js
@@ -1,11 +1,35 @@
 import React from "react";
+import "./Projects.css";
 
-function ProjectNavigation() {
+function Projects() {
   return (
-    <div>
-      <Link to="current">Game 1</Link>
-      <Link to="current">Game 2</Link>
-      <Link to="current">Game 3</Link>
+    <div id="projects">
+      <h2 className="center blue-robotic-text">Projects</h2>
+      <div className="project-cards">
+        <div className="card">
+          <h3 className="project-title">FantasyPop</h3>
+          <div className="project-tech-stack"></div>
+          {/* Tech Stack: React Native, Java/Spring, MySQL, AWS */}
+          <p className="project-description"></p>
+          {/* <img /> */}
+        </div>
+        <div className="card">
+          <h3 className="project-title">Tik-Tac-Toe</h3>
+          {/* Tech Stack: HTML5, CSS, Javascript */}
+          <div className="project-tech-stack"></div>
+          <p className="project-description"></p>
+          {/* <img /> */}
+        </div>
+        <div className="card">
+          <h3 className="project-title"> Olivia's RRS</h3>
+          {/* Tech Stack: React, HTML/CSS, Node.js, PostgresSQL */}
+          <div className="project-tech-stack"></div>
+          <p className="project-description"></p>
+          {/* <img /> */}
+        </div>
+      </div>
     </div>
   );
 }
+
+export default Projects;
