8a05aba2a957aa169ad858155108e61bbdf7f6ea
James-Ukoli <thejamesukoli@gmail.com>
2023-10-03 11:02:43 -0500

    almost close to mobile-navigation

--- src/sections/ButtonNavigation.js
+++ src/sections/ButtonNavigation.js
@@ -6,16 +6,16 @@ function ButtonNavigation() {
 
   return (
     <div className="scrollable-div Button-container">
-      <a href="#projects" className="Robot-button">
+      <a href="#projects" className="Robot-button item">
         Projects📑
       </a>
-      <a href="#skills" className="Robot-button">
+      <a href="#skills" className="Robot-button item">
         Skills🖥️
       </a>
-      <a href="#hobbies" className="Robot-button">
+      <a href="#hobbies" className="Robot-button item">
         Hobbies🎨
       </a>
-      <a href="#contactme" className="Robot-button">
+      <a href="#contactme" className="Robot-button item">
         Contact Me✉️
       </a>
     </div>
