caaf401bf6a8efd9c5d6f7bd32e9fad71dae44ed
James-Ukoli <thejamesukoli@gmail.com>
2023-10-05 08:25:48 -0500

    seeable navigation

--- src/sections/ButtonNavigation.js
+++ src/sections/ButtonNavigation.js
@@ -5,19 +5,21 @@ function ButtonNavigation() {
   // Scroll to a specific section when a button is clicked
 
   return (
-    <div className="scrollable-div Button-container">
-      <a href="#projects" className="Robot-button item">
-        Projects📑
-      </a>
-      <a href="#skills" className="Robot-button item">
-        Skills🖥️
-      </a>
-      <a href="#hobbies" className="Robot-button item">
-        Hobbies🎨
-      </a>
-      <a href="#contactme" className="Robot-button item">
-        Contact Me✉️
-      </a>
+    <div className="scrollable-container">
+      <div className="scrollable-div">
+        <a href="#projects" className="Robot-button">
+          Projects📑
+        </a>
+        <a href="#skills" className="Robot-button">
+          Skills🖥️
+        </a>
+        <a href="#hobbies" className="Robot-button">
+          Hobbies🎨
+        </a>
+        <a href="#contactme" className="Robot-button">
+          Contact Me✉️
+        </a>
+      </div>
     </div>
   );
 }
