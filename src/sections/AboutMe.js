04ab8485ad1a4452a73d2e37bb4c039927f64aa6
James-Ukoli <thejamesukoli@gmail.com>
2024-05-20 23:31:32 -0500

    background transformers image added

--- src/sections/AboutMe.js
+++ src/sections/AboutMe.js
@@ -6,11 +6,13 @@ function AboutMe() {
     <>
       <div id="aboutme" className="">
         <h2 className="center blue-robotic-text element1">About Me</h2>
-        <div className="white-robotic-text paragraph uppity">
-          Hello there! I'm The James Ukoli! I am a Mathematics Educator that has
-          a deep passion for creating software. I enjoy the mobile development
-          space the most, and I love to explore tech and robotics during my
-          freetime. Scroll down for more things about me :)
+        <div className="paragraphContainer">
+          <div className="white-robotic-text paragraph">
+            Hello there! I'm The James Ukoli! I am a Mathematics Educator that
+            has a deep passion for creating software. I enjoy the mobile
+            development space the most, and I love to explore tech and robotics
+            during my freetime. Scroll down for more things about me :)
+          </div>
         </div>
       </div>
     </>
