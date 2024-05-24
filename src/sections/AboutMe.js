215d9cbc6faa6977c6e6b913be4c6acec83d814f
James-Ukoli <thejamesukoli@gmail.com>
2024-05-24 10:26:33 -0500

    about me update

--- src/sections/AboutMe.js
+++ src/sections/AboutMe.js
@@ -8,10 +8,12 @@ function AboutMe() {
         <h2 className="center blue-robotic-text element1">About Me</h2>
         <div className="paragraphContainer">
           <div className="white-robotic-text paragraph">
-            Hello there! I'm The James Ukoli! I am a Mathematics Educator that
-            has a deep passion for creating software. I enjoy the mobile
-            development space the most, and I love to explore tech and robotics
-            during my freetime. Scroll down for more things about me :)
+            Hello there! I'm The James Ukoli! I am an Middle School Mathematics
+            Educator that has a deep passion for creating SOFTWARE. I enjoy
+            working on front end mobile apps the most, as it showcases my
+            creative and stylistic side. During my freetime, I like to explore
+            and research on new tech and robotics. Scroll down for more things
+            about me and my work. :)
           </div>
         </div>
       </div>
